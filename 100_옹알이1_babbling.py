f3f1cd9f42730c87c95c024c918236edc40a1b8e
Jiyoung July Oh <31668639+JiJuOh@users.noreply.github.com>
2023-01-28 12:34:35 +0900

    Update 100_옹알이1_babbling.py

--- 100_옹알이1_babbling.py
+++ 100_옹알이1_babbling.py
@@ -1,4 +1,10 @@
-# yemaye가 안됨
+# https://school.programmers.co.kr/learn/courses/30/lessons/120956
+# 제한사항 - babbling의 각 문자열에서 "aya", "ye", "woo", "ma"는 각각 최대 한 번씩만 등장합니다
+# Restrictions - In a string in babbling, "aya", "ye", "woo", "ma" occur at most one time.
+# my code works when the babbling occurence in a word is two.
+# if a word has over two times of occurence like yemaye, it can't catch the word.
+# I was trying to change it recursive. 
+
 
 def checkBabble(x):
     if "aya" in x:
@@ -41,6 +47,15 @@ def solution(babbling):
 # 테스트 15 〉	통과 (0.02ms, 10.4MB)
 # 테스트 16 〉	통과 (0.02ms, 10.3MB)
 # 테스트 17 〉	통과 (0.03ms, 10.2MB)
+
+# Below is the one of the other answers
+# I tried like this at first, but it always made more elements than the original babbling list.
+# string.replace(oldvalue, newvalue, count)
+# Count is optional. A number specifying how many occurrences of the old value you want to replace. Default is all occurrences.
+# after replacing possible babbling with a space,
+# remove spaces with strip().
+# when we find an element which length is 0, increase c by 1
+
 
 def solution(babbling):
     c = 0
